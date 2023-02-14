60fb14cc3806012bc1c0e3e662a49878c2b609b5
Amruta-26 <darakamruta@gmail.com>
2023-02-14 12:21:41 +0530

    Added Our services section

--- src/Screens/LandingPage/LandingPage.js
+++ src/Screens/LandingPage/LandingPage.js
@@ -2,23 +2,24 @@ import React from "react";
 import "./LandingPage.css";
 import NavigationBar from "../../Components/NavigationBar/NavigationBar";
 import Banner from "../../Components/Banner/Banner";
+import Service from "../../Components/Service/Service"
 // import Footer from "../../Components/Footer/Footer"
 // import Process from "../../Components/Process/Process";
 
 const LandingPage = () => {
   return (
-        <div>
-
-          <NavigationBar className="nav" />
-       
-          {/* we need a banner component which will be placed just after the navbar */}
-          <Banner className="banner" />
-          {/* <h2 className="text-center ">Our Process</h2> */}
-          {/* <Process className="process" /> */}
-  
-          {/* footer */}
-          {/* <Footer /> */}
-        </div>
+    <div>
+      <div className="nav-banner">
+        <NavigationBar className="nav" />
+        {/* we need a banner component which will be placed just after the navbar */}
+        <Banner className="banner" />
+      </div>
+      <br />
+      <div className="sevices">
+        <Service className="service"/>
+      </div>
+      <br/>
+    </div>
   );
 };
 
